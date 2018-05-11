992f62fb8b87f723938c1d6f97aa73d8aff11dff
Ivan Bondarenko <bond005@yandex.ru>
2018-05-11 14:32:48 +0700

    `setup.py` has been modified to better adapt for Windows.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def compile_test(header, library):
     return os.system(command) == 0
 
 
-FILES = glob.glob('util/*.cc') + glob.glob('lm/*.cc') + glob.glob('util/double-conversion/*.cc')
+FILES = glob.glob(os.path.join('util', '*.cc')) + glob.glob(os.path.join('lm', '*.cc')) + glob.glob(os.path.join('util', 'double-conversion/*.cc'))
 FILES = [fn for fn in FILES if not (fn.endswith('main.cc') or fn.endswith('test.cc'))]
 
 if platform.system() == 'Linux':
@@ -21,23 +21,27 @@ else:
     LIBS = []
 
 #We don't need -std=c++11 but python seems to be compiled with it now.  https://github.com/kpu/kenlm/issues/86
-ARGS = ['-O3', '-DNDEBUG', '-DKENLM_MAX_ORDER=6', '-std=c++11']
+if platform.system() == 'Windows':
+    ARGS = ['/O2', '/DNDEBUG', '/DKENLM_MAX_ORDER=6', '/D_CRT_NONSTDC_NO_WARNINGS']
+else:
+    ARGS = ['-O3', '-DNDEBUG', '-DKENLM_MAX_ORDER=6', '-std=c++11']
 
-if compile_test('zlib.h', 'z'):
-    ARGS.append('-DHAVE_ZLIB')
-    LIBS.append('z')
+if platform.system() != 'Windows':
+    if compile_test('zlib.h', 'z'):
+        ARGS.append('-DHAVE_ZLIB')
+        LIBS.append('z')
 
-if compile_test('bzlib.h', 'bz2'):
-    ARGS.append('-DHAVE_BZLIB')
-    LIBS.append('bz2')
+    if compile_test('bzlib.h', 'bz2'):
+        ARGS.append('-DHAVE_BZLIB')
+        LIBS.append('bz2')
 
-if compile_test('lzma.h', 'lzma'):
-    ARGS.append('-DHAVE_XZLIB')
-    LIBS.append('lzma')
+    if compile_test('lzma.h', 'lzma'):
+        ARGS.append('-DHAVE_XZLIB')
+        LIBS.append('lzma')
 
 ext_modules = [
     Extension(name='kenlm',
-        sources=FILES + ['python/kenlm.cpp'],
+        sources=FILES + [os.path.join('python', 'kenlm.cpp')],
         language='C++', 
         include_dirs=['.'],
         libraries=LIBS, 
